6c699187b684969397d520b7aa1ee3f12a5b8967
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2021-11-09 14:01:49 +0900

    renamed some file and studied memory overflow when used %[^\n]

---
+++ 열혈C/열혈C/question-twenty-three-two.c
@@ -0,0 +1,14 @@
+//
+//  question-twenty-three-two.c
+//  열혈C
+//
+//  Created by 안동규 on 2021/11/09.
+//  열혈 488p 문제
+
+#include <stdio.h>
+
+int main()
+{
+    
+    return 0;
+}
--- 열혈C/열혈C/text-datafile-write.c
+++ 열혈C/열혈C/text-datafile-write.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-int main()
+int main_text_data_file_write()
 {
     char readtxt[20];
     int ch;
@@ -18,18 +18,19 @@ int main()
         return -1;
     }
     
-    while((ch=fgetc(rtfp))!=EOF)
-        fputc(ch, wtfp);
+    while((ch=fgetc(rtfp))!=EOF)  // end of file이 나올때까지 불러와서
+        fputc(ch, wtfp);  // 새로운 파일에 붙여넣기
     
-    if(feof(rtfp)!=0)
+    if(feof(rtfp)!=0)  // 대신에 feof를 통해서 end of file이 오류로 인한 것인지 아닌지를 판단해준다! 정상 작동을 하면 0이 '아닌' 값을 반환해준다.
         puts("파일복사 완료!\n\n");
     else
         puts("파일복사 실패!\n\n");
     
-    if(fclose(rtfp)!=0 || fclose(wtfp)!=0)
+    if(fclose(rtfp)!=0 || fclose(wtfp)!=0)  // 이건 다른 블로그에서 봤는데 fclose를 했을때 0이 반환되면 정상 종료, 아니면 스트림 제거 실패!
     {
         puts("스트림 제거시 실패\n\n");
         return 1;
     }
     return 0;
 }
+
