68bbd5eeb7d5d1c48b55705c2271f0ecc58af3eb
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2021-11-15 16:07:44 +0900

    Studied bja

--- bja/bja/main.c
+++ bja/bja/main.c
@@ -6,6 +6,7 @@
 //  백준 문제들
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     // 2588번
@@ -51,18 +52,52 @@ int main() {
 //        printf("0\n");
     
     // 14681
-    int x, y;
-    scanf("%d", &x);
-    scanf("%d", &y);
+//    int x, y;
+//    scanf("%d", &x);
+//    scanf("%d", &y);
+//
+//    if(x>0 && y>0)
+//        printf("1\n");
+//    else if(x<0 && y>0)
+//        printf("2\n");
+//    else if(x<0 && y<0)
+//        printf("3\n");
+//    else
+//        printf("4\n");
     
-    if(x>0 && y>0)
-        printf("1\n");
-    else if(x<0 && y>0)
-        printf("2\n");
-    else if(x<0 && y<0)
-        printf("3\n");
-    else
-        printf("4\n");
+    // 2884
+//    int hours, mins, temp;
+//    scanf("%d %d", &hours, &mins);
+//    temp = (hours*60+mins)-45;
+//    if(temp<0){
+//        hours = 23;
+//        mins = 60-abs(temp%60);
+//        printf("%d %d",hours,mins);
+//    } else {
+//        hours = temp/60;
+//        mins = temp%60;
+//        printf("%d %d",hours,mins);
+//    }
     
+    // 2739
+//    int n;
+//    scanf("%d",&n);
+//    for(int i=1;i<10;i++)
+//        printf("%d * %d = %d\n", n, i, n*i);
+    
+    // 10950
+//    int i,a,b;
+//    scanf("%d", &i);
+//    for(int j=0;j<i;j++){
+//        scanf("%d %d", &a, &b);
+//        printf("%d\n",a+b);
+//    }
+    
+    // 8393
+    int i, sum=0;
+    scanf("%d", &i);
+    for(int j=0;j<=i;j++)
+        sum +=j;
+    printf("%d",sum);
     return 0;
 }
