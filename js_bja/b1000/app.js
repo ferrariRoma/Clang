bdf21237a99ffa8923a716ad1902ffc6c74f9b02
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-03-21 03:02:24 +0900

    Solved baekjoon no.1000. Used readline, Not fs

--- js_bja/b1000/app.js
+++ js_bja/b1000/app.js
@@ -1,11 +1,16 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/div/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-input = input[0];
-input = input.split(" ").map((num) => +num);
+let data = null;
 
-solution(input[0], input[1]);
-function solution(A, B) {
-  console.log(A + B);
-}
+rl.on("line", (line) => {
+  data = line.split(" ").map((item) => +item);
+  const a = data[0];
+  const b = data[1];
+  const sum = a + b;
+  console.log(sum);
+  rl.close();
+});
