4349a50217c66f6f8bb5b109d766c97b7e7c6366
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-04-15 03:05:31 +0900

    Solved insertion sort question

---
+++ js_bja/zerocho/insert/app.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+  const crude = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (crude[i] < crude[j]) {
+        const temp = crude[i];
+        crude[i] = crude[j];
+        crude[j] = temp;
+      }
+    }
+  }
+  const answer = crude;
+  console.log(answer);
+  return answer;
+}
+solution([5, 6, 1, 2, 4, 3]);
